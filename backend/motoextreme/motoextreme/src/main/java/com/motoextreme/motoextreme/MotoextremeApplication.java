31b8ecde2cc568d9461cd6031ae5158e6db3c3af
daniel galindo <danielmauriciogalindo07@gmail.com>
2025-11-19 09:04:41 -0500

    backend

---
+++ backend/motoextreme/motoextreme/src/main/java/com/motoextreme/motoextreme/MotoextremeApplication.java
@@ -0,0 +1,13 @@
+package com.motoextreme.motoextreme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoextremeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotoextremeApplication.class, args);
+	}
+
+}
---
+++ backend/motoextreme/motoextreme/src/test/java/com/motoextreme/motoextreme/MotoextremeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motoextreme.motoextreme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotoextremeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
