0becceed889098e2474e0dd3dbc544fc3215c83c
Isabella Bernal <bellabernalcarvajal871@gmail.com>
2025-11-24 19:14:01 -0500

    enrutamiendo de páginas en un header

--- motos/src/app/app.routes.ts
+++ motos/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './pages/inicio/inicio';
+import { Modelos } from './pages/modelos/modelos';
+import { Configurador } from './pages/configurador/configurador';
+import { Accesorios } from './pages/accesorios/accesorios';
+import { Contacto } from './pages/contacto/contacto';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: Inicio},
+  {path: 'modelos', component: Modelos},
+  {path: 'configurador', component: Configurador},
+  {path: 'accesorios', component: Accesorios},
+  {path: 'contacto', component: Contacto}
+];
